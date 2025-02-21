d3310311c8f7d36c8d9fec27245cf4aefb3b372c
Rafal Mi <rafal.m4@gmail.com>
2025-02-21 12:13:17 +0100

    Loop cut first time

---
+++ edit-mode/loop_cut.py
@@ -0,0 +1,46 @@
+import bpy 
+import bmesh 
+
+# Must start in object mode 
+bpy.ops.object.mode_set(mode='OBJECT') 
+bpy.ops.object.select_all(action='SELECT') 
+bpy.ops.object.delete() 
+
+# Create a cube and enter Edit Mode 
+bpy.ops.mesh.primitive_cube_add(size=1, location=(0, 0, 0)) 
+bpy.ops.object.mode_set(mode='EDIT') 
+
+bpy.ops.mesh.loopcut_slide(
+    MESH_OT_loopcut={
+        "number_cuts":3, 
+        "smoothness":0, 
+        "falloff":
+        'INVERSE_SQUARE', 
+        "object_index":0, 
+        "edge_index":11, 
+        "mesh_select_mode_init":(True, False, False)
+    }, 
+    TRANSFORM_OT_edge_slide={
+        "value":-0.414013, 
+        "single_side":False, 
+        "use_even":False, 
+        "flipped":False, 
+        "use_clamp":True, 
+        "mirror":True, 
+        "snap":False, 
+        "snap_elements":{
+            'INCREMENT'
+        }, 
+        "use_snap_project":False, 
+        "snap_target":'CLOSEST', 
+        "use_snap_self":True, 
+        "use_snap_edit":True, 
+        "use_snap_nonedit":True, 
+        "use_snap_selectable":False, 
+        "snap_point":(0, 0, 0), 
+        "correct_uv":True, 
+        "release_confirm":False, 
+        "use_accurate":False
+    }
+)
+
